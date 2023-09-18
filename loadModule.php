3488ff17f62820c702e8fe49512324d2d12fd45a
Andre van Zuydam <andrevanzuydam@gmail.com>
2023-09-18 18:22:57 +0200

    Fix for load module

--- loadModule.php
+++ loadModule.php
@@ -4,13 +4,11 @@ use Twig\TwigFunction;
 
 \Tina4\Module::addModule("Documentation Module", "1.0.0", "tina4documentation", static function (\Tina4\Config $config) {
 
-
+    //Deploy the SCSS for the documentation
     $scss = new ScssPhp\ScssPhp\Compiler();
-
     $scssDefault = $scss->compileString(file_get_contents(__DIR__."/src/templates/documentation/default.scss"))->getCss();
-    file_put_contents(realpath("./src/public/css/tina4-docs.css"), $scssDefault);
+    file_put_contents("./src/public/css/tina4-docs.css", $scssDefault);
 
-    //do something here specific with documentation ...
     $config->addTwigFunction("include_code",  function($fileName) {
         $fileName = __DIR__."/src/templates/documentation/".$fileName;
         if (file_exists($fileName)) {
