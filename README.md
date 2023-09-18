4edb6da1fe3aa2830fa7b2dd5209b74a74842983
Andre van Zuydam <andrevanzuydam@gmail.com>
2023-09-18 12:23:52 +0200

    Fix for composer and loadModule

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# tina4-documentation" 
+# Tina4 Documentation Module
+
+This module adds Tina4 documentation to your project, so you can reference it under /documentation.
+
+```
+composer require tina4stack/tina4-documentation --dev
+```
+It is recommended that you add the --dev tag so the documentation is only included when you are busy developing.
--- loadModule.php
+++ loadModule.php
@@ -4,7 +4,7 @@ use Twig\TwigFunction;
 
 \Tina4\Module::addModule("Documentation Module", "1.0.0", "tina4documentation", static function (\Tina4\Config $config) {
     //do something here specific with documentation ...
-    $config->addTwigFunction("include_code_1",  function($fileName) {
+    $config->addTwigFunction("include_code",  function($fileName) {
         $fileName = "./src/templates/documentation/".$fileName;
         if (file_exists($fileName)) {
             return file_get_contents($fileName);
