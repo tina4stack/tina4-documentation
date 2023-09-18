c3d3290cf274d2d225c8e1e912773e91f5f41318
Andre van Zuydam <andrevanzuydam@gmail.com>
2023-09-18 10:14:35 +0200

    New documentation repository push

---
+++ loadModule.php
@@ -0,0 +1,4 @@
+<?php
+\Tina4\Module::addModule("Documentation Module", "1.0.0", "tina4documentation", static function (\Tina4\Config $config) {
+    //do something here specific with documentation ...
+});
