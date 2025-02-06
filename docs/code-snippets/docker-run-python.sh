b21d7489a9ac2526003dcda05c514c20780f9347
Andre van Zuydam <andrevanzuydam@gmail.com>
2025-02-06 09:30:31 +0200

    Added updated documentation for Python

--- docs/code-snippets/docker-run-python.sh
+++ docs/code-snippets/docker-run-python.sh
@@ -8,4 +8,4 @@ docker run -v %cd%:/app -p"7145:7145" tina4stack/python:latest python -u app.py 
 docker run -v $(pwd):/app tina4stack/python:latest poetry init
 docker run -v $(pwd):/app tina4stack/python:latest poetry add tina4-python
 echo import tina4_python > app.py
-docker run -v $(pwd):/app -p"7145:7145" tina4stack/python:latest python -u app.py
+docker run -v $(pwd):/app -p"7145:7145" tina4stack/python:latest python -u app.py 0.0.0.0:7145
